a0ae170d8cb13ea2bd2727af5064f88874291169
Alexander Koval <kovalidis@gmail.com>
2022-12-02 12:28:01 +0200

    Day 1, puzzle 2

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::BinaryHeap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -6,20 +7,21 @@ use anyhow::Result as AnyResult;
 fn main() -> AnyResult<()> {
     let input = BufReader::new(File::open("day-01/input.txt")?);
 
-    let mut max_calories = 0u64;
+    let mut top3_elves_calories = BinaryHeap::new();
     let mut cur_elf_calories = 0u64;
     for line in input.lines() {
         let row = line?;
         if row.is_empty() {
-            if cur_elf_calories > max_calories {
-                max_calories = cur_elf_calories;
-            }
+            top3_elves_calories.push(cur_elf_calories);
             cur_elf_calories = 0;
             continue;
         }
         cur_elf_calories += row.parse::<u64>()?;
     }
 
+    let max_calories = top3_elves_calories.peek().unwrap();
     println!("{}", max_calories);
+
+    println!("{}", top3_elves_calories.iter().take(3).sum::<u64>());
     Ok(())
 }
