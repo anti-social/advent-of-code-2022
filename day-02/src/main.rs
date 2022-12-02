c03129ec52a5ac8a0f598cf6e5fe3d66e78d49ee
Alexander Koval <kovalidis@gmail.com>
2022-12-02 19:45:33 +0200

    Day 2, puzzle 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -7,21 +7,31 @@ use anyhow::{bail, Result as AnyResult};
 fn main() -> AnyResult<()> {
     let input = BufReader::new(File::open("day-02/input.txt")?);
 
-    let score = input.lines()
-        .map(|line| {
-            let row = line.unwrap();
-            let mut encoded_shapes = row.split_whitespace();
-            let their_shape = encoded_shapes.next().unwrap().parse::<Shape>().unwrap();
-            let mine_shape = encoded_shapes.next().unwrap().parse::<Shape>().unwrap();
-            mine_shape.score(&their_shape)
-        })
-        .sum::<u64>();
+    let mut score1 = 0u64;
+    let mut score2 = 0u64;
+    for line in input.lines() {
+        let row = line?;
+        let mut encoded_shapes = row.split_whitespace();
+        let their_shape = encoded_shapes.next().unwrap().parse::<Shape>()?;
+        let encoded = encoded_shapes.next().unwrap();
+        let mine_shape = encoded.parse::<Shape>()?;
+        score1 += mine_shape.score(&their_shape);
 
-    println!("{}", score);
+        let mine_shape = match encoded.parse::<Round>()? {
+            Round::Lose => their_shape.losing_opponent(),
+            Round::Draw => their_shape.draw_opponent(),
+            Round::Win => their_shape.winning_opponent(),
+        };
+        score2 += mine_shape.score(&their_shape);
+    }
+
+    println!("{}", score1);
+    println!("{}", score2);
 
     Ok(())
 }
 
+#[derive(Clone, PartialEq)]
 enum Shape {
     Rock,
     Paper,
@@ -29,6 +39,34 @@ enum Shape {
 }
 
 impl Shape {
+    const BEATS: &'static [(Shape, Shape)] = &[
+        (Shape::Rock, Shape::Scissors),
+        (Shape::Paper, Shape::Rock),
+        (Shape::Scissors, Shape::Paper),
+    ];
+
+    fn winning_opponent(&self) -> Self {
+        for (oppo, mine) in Self::BEATS {
+            if mine == self {
+                return oppo.clone();
+            }
+        }
+        unreachable!()
+    }
+
+    fn draw_opponent(&self) -> Self {
+        self.clone()
+    }
+
+    fn losing_opponent(&self) -> Self {
+        for (mine, oppo) in Self::BEATS {
+            if mine == self {
+                return oppo.clone();
+            }
+        }
+        unreachable!()
+    }
+
     fn score(&self, their: &Shape) -> u64 {
         use Shape::*;
 
@@ -37,10 +75,12 @@ impl Shape {
             Paper => 2,
             Scissors => 3,
         };
-        let round_score = match (self, their) {
-            (Rock, Paper) | (Paper, Scissors) | (Scissors, Rock) => 0,
-            (Paper, Rock) | (Scissors, Paper) | (Rock, Scissors) => 6,
-            (Rock, Rock) | (Paper, Paper) | (Scissors, Scissors) => 3,
+        let round_score = if self.winning_opponent() == *their {
+            0
+        } else if self.losing_opponent() == *their {
+            6
+        } else {
+            3
         };
         return shape_score + round_score;
     }
@@ -55,6 +95,25 @@ impl FromStr for Shape {
             "B" | "Y" => Shape::Paper,
             "C" | "Z" => Shape::Scissors,
             _ => bail!("Unknown shape: {}", s),
+        })
+    }
+}
+
+enum Round {
+    Lose,
+    Draw,
+    Win,
+}
+
+impl FromStr for Round {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "X" => Round::Lose,
+            "Y" => Round::Draw,
+            "Z" => Round::Win,
+            _ => bail!("Unknown round result: {}", s),
         })
     }
 }
